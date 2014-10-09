e6b311325065e563da31ee2c0f31798d9d9b4976
ryutamaki <ryu.mochi@gmail.com>
2014-10-09 23:13:26 +0900

    Initialize project

---
+++ TMKLocationChaser/TMKLocationChaser/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TMKLocationChaser
+//
+//  Created by ryutamaki on 2014/10/09.
+//  Copyright (c) 2014年 Ryu Tamaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
