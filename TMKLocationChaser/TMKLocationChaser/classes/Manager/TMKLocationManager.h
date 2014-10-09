66409087cdbd488e5f76f51cbcc1f7cb10032cc6
ryutamaki <ryu.mochi@gmail.com>
2014-10-09 23:50:19 +0900

    Add "Classes" directory and move files into it

---
+++ TMKLocationChaser/TMKLocationChaser/classes/Manager/TMKLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  TMKLocationManager.h
+//  TMKLocationChaser
+//
+//  Created by ryutamaki on 2014/10/09.
+//  Copyright (c) 2014年 Ryu Tamaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMKLocationManager : NSObject
+
+@end
---
+++ TMKLocationChaser/TMKLocationChaser/classes/Manager/TMKLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  TMKLocationManager.m
+//  TMKLocationChaser
+//
+//  Created by ryutamaki on 2014/10/09.
+//  Copyright (c) 2014年 Ryu Tamaki. All rights reserved.
+//
+
+#import "TMKLocationManager.h"
+
+@implementation TMKLocationManager
+
+@end
